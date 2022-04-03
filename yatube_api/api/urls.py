d4203200dc21d6052d404d16979dcd2a4684e3a2
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-04-03 17:03:38 +0300

    homework

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework.authtoken import views
+from rest_framework import routers
+
+from .views import PostViewSet, CommentViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
+]
---
+++ yatube_api/api/views.py
@@ -0,0 +1,52 @@
+from rest_framework import viewsets, permissions
+from django.core.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets
+
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from posts.models import Post, Group, Comment
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Пермишн для проверки, является ли юзер автором поста"""
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """Вьюсет для обработки CRUD для posts"""
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для обработки CRUD для groups"""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет для обработки CRUD для comments"""
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+def perform_update(self, serializer):
+    if serializer.instance.author != self.request.user:
+        raise PermissionDenied('Изменение чужого контента запрещено!')
+    super(PostViewSet, self).perform_update(serializer)
